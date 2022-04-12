bcd40fb2a569098e66a47e86df872fae665190ef
Roberto Conte Rosito <roberto.conterosito@gmail.com>
2022-04-12 17:11:16 +0200

    Fix: updated install.sh.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-composer create-project --prefer-dist cakephp/app:~4.0 /var/www/html/app --no-interaction
-mv /var/www/html/app/* /var/www/html/
+composer create-project --prefer-dist cakephp/app:4.2 /var/www/app --no-interaction
+mv /var/www/app/* /var/www/html/
